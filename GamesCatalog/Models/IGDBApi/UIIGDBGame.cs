328a4bb7ee395953eaabe2295083f47c868d6921
Emanuel <emanuel.xpe@gmail.com>
2025-01-17 08:53:04 -0300

    Criação da model para o grid de buscas

---
+++ GamesCatalog/Models/IGDBApi/UIIGDBGame.cs
@@ -0,0 +1,13 @@
+﻿namespace GamesCatalog.Models.IGDBApi
+{
+    public class UIIGDBGame
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string CoverUrl { get; set; }
+    }
+}
